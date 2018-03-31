ef09237f022d890fd94db7c816381325586d0d54
Stiff Roy <stiff.roy@gmail.com>
2018-03-31 23:59:59 +0200

    New database seeder
    New database seeder based on the Laravel role package

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Ultraware\Roles\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /**
+         * Add Roles
+         */
+        if (Role::where('name', '=', 'Admin')->first() === null) {
+            $adminRole = Role::create([
+                'name' => 'Admin',
+                'slug' => 'admin',
+                'description' => 'Admin Role',
+                'level' => 5,
+            ]);
+        }
+
+        if (Role::where('name', '=', 'User')->first() === null) {
+            $userRole = Role::create([
+                'name' => 'User',
+                'slug' => 'user',
+                'description' => 'User Role',
+                'level' => 1,
+            ]);
+        }
+
+        if (Role::where('name', '=', 'Unverified')->first() === null) {
+            $userRole = Role::create([
+                'name' => 'Unverified',
+                'slug' => 'unverified',
+                'description' => 'Unverified Role',
+                'level' => 0,
+            ]);
+        }
+    }
+}
