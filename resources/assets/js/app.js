0ea93fffa74d1692b7a4bdbac2f7c94f110dad22
Stiff Roy <stiff.roy@gmail.com>
2018-05-02 23:14:56 +0200

    JS files based on Vue technology

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +7,31 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
+/**
+ * Importing the Vue2 Admin LTE
+ */
+import 'vue2-admin-lte/src/lib/css';
+import 'vue2-admin-lte/src/lib/script';
+import Home from './components/Home';
+import Users from './components/users/UsersOverview';
+import UsersList from './components/users/UsersList';
+import UserCreate from './components/users/UsersCreate';
+import UserShow from './components/users/UsersShow';
+import UserEdit from './components/users/UsersEdit';
+import Groups from './components/groups/GroupsOverview';
+import GroupsList from './components/groups/GroupsList';
+import GroupCreate from './components/groups/GroupsCreate';
+import GroupShow from './components/groups/GroupsShow';
+import GroupEdit from './components/groups/GroupsEdit';
+import Clients from './components/clients/ClientsOverview';
+import ClientsList from './components/clients/ClientsList';
+import ClientCreate from './components/clients/ClientsCreate';
+import ClientShow from './components/clients/ClientsShow';
+import ClientEdit from './components/clients/ClientsEdit';
+
+window.Vue.use(VueRouter);
+Vue.use(require('vue-moment'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +39,28 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('side-menu', require('./components/SideMenu'));
+// Vue.component(ClientsList, require('./components/ClientsList.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+const routes = [
+    {path: '/', component: Home, name: 'home'},
+    {path: '/users', component: Users, name: 'users'},
+    {path: '/users/list', component: UsersList, name: 'listUsers'},
+    {path: '/users/create', component: UserCreate, name: 'createUser'},
+    {path: '/users/show/:id', component: UserShow, name: 'showUser'},
+    {path: '/users/edit/:id', component: UserEdit, name: 'editUser'},
+    {path: '/groups', component: Groups, name: 'groups'},
+    {path: '/groups/list', component: GroupsList, name: 'listGroups'},
+    {path: '/groups/create', component: GroupCreate, name: 'createGroup'},
+    {path: '/groups/show/:id', component: GroupShow, name: 'showGroup'},
+    {path: '/groups/edit/:id', component: GroupEdit, name: 'editGroup'},
+    {path: '/clients', component: Clients, name: 'clients'},
+    {path: '/clients/list', component: ClientsList, name: 'listClients'},
+    {path: '/clients/create', component: ClientCreate, name: 'createClient'},
+    {path: '/clients/show/:id', component: ClientShow, name: 'showClient'},
+    {path: '/clients/edit/:id', component: ClientEdit, name: 'editClient'},
+];
+
+const router = new VueRouter({ routes });
+
+const app = new Vue({ router }).$mount('#app');
