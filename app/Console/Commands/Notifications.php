3fbef065c7c95eacc64174a74d309826ce4b38cc
stiff.roy <stiff.roy@gmail.com>
2018-08-15 22:00:38 +0200

    Rough notification table

---
+++ app/Console/Commands/Notifications.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Entity\Client;
+use App\Notifications\ClientHealth;
+use App\Utilities\ClientUtilities;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+
+class Notifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notifications:health';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'To periodically check and notify the users when a client health is in danger';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::debug('In Notifications:handle at: ' . now());
+        $clients = Client::with('alive', true)->get();
+
+        foreach ($clients as $client)
+        {
+            if ($client->health === ClientUtilities::CRITICAL) {
+                $this->sendNotification($client);
+            }
+        }
+    }
+
+    private function sendNotification($client)
+    {
+        $users = $client->group_id ? $client->group->users: new Collection();
+
+        if (count($users) > 0) {
+            Notification::send($users, new ClientHealth($client));
+        }
+    }
+}
---
+++ app/Notifications/ClientHealth.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Notifications;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
+
+class ClientHealth extends Notification
+{
+    use Queueable;
+
+    const BASE_URL = 'http://www.dashboard.avc/#/clients/show/';
+    private $client;
+
+    /**
+     * ClientHealth constructor.
+     * @param $client
+     */
+    public function __construct($client)
+    {
+        Log::debug('In ClientHealth:__construct at: ' . now());
+        $this->client = $client;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'slack', 'broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->error()
+            ->subject('VM in danger')
+            ->greeting('Hello,')
+            ->line('VM ' . $this->client->name . ' is in danger.')
+            ->line('Check-up time at: ' . Carbon::now('Europe/Vienna')->format('d.m.Y H:i'))
+            ->action('Notification Action', url($this::BASE_URL . $this->client->id))
+            ->line('Thank you.');
+    }
+
+    /**
+     * Get the broadcastable representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return BroadcastMessage
+     */
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'client_id' => $this->client->id,
+            'health' => $this->client->health,
+        ]);
+    }
+
+    /**
+     * Get the Slack representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return SlackMessage
+     */
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->error()
+            ->content('VM ' . $this->client->name . ' is in danger.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
